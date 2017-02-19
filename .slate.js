d96ff2a4907ab0697047577f27cb06dbf79bea4f
lumberingAutomaton <lumberingAutomaton@gmail.com>
2017-02-19 14:18:55 -0800

    slate complete revamp on position detection. This phase is fuzzy detection, where we can get a basic idea of where our window is.

--- .slate.js
+++ .slate.js
@@ -1,8 +1,8 @@
 // slate js configuration
-
+/* globals slate */
 (function () {
     'use strict';
-    var screen, view, positions, sizes;
+
     var directions = {
         left: 0,
         right: 1,
@@ -10,214 +10,175 @@
         down: 1
     };
 
-    var update = function () {
-        screen = slate.screen();
-        view = screen.rect();
-        //view = screen.visibleRect();
-        positions = {
-            horizontal: {
-                left: view.x,
-                center: view.width / 4,
-                right: view.width / 2
-            },
-            vertical: {
-                top: view.y,
-                bottom: view.height / 2 + 12
-            }
-        };
-
-        sizes = {
-            width: {
-                half: view.width / 2,
-                full: view.width
-            },
-            height: {
-                half: view.height / 2 - 12,
-                full: view.height
-            }
-        };
+    var Point = function (x, y) {
+        this.x = x;
+        this.y = y;
     };
 
-    var roughly = function (over, under) {
-        var proportion = over / under;
-        return (proportion < 1.05) && proportion > 0.95;
+    Point.max = function (p1, p2) {
+        return new Point(
+            Math.max(p1.x, p2.x),
+            Math.max(p1.y, p2.y)
+        );
     };
 
-    var toFull = function (win) {
-        win.resize({
-            width: sizes.width.full,
-            height: sizes.height.full
-        });
+    Point.min = function (p1, p2) {
+        return new Point(
+             Math.min(p1.x, p2.x),
+             Math.min(p1.y, p2.y)
+        );
     };
 
-    var isFull = function (rect) {
-        return roughly(rect.width, sizes.width.full);
+    var Rect = function (tl, br) {
+        this.tl = tl;
+        this.br = br;
     };
 
-    var toHalf = function (win) {
-        win.resize({
-            width: sizes.width.half,
-            height: sizes.height.full
-        });
+    Rect.prototype.area = function () {
+        var w = this.br.x - this.tl.x;
+        var h = this.br.y - this.tl.y;
+        return w * h;
     };
 
-    var isHalf = function (rect) {
-        return roughly(rect.width, sizes.width.half) &&
-            roughly(rect.height, sizes.height.full);
-    };
-
-    var toQuarter = function (win) {
-        win.resize({
-            width: sizes.width.half,
-            height: sizes.height.half
-        });
-    };
-
-    var isQuarter = function (rect) {
-        return (isTop(rect) || isBottom(rect)) && roughly(rect.width, sizes.width.half) &&
-            roughly(rect.height, sizes.height.half);
-    };
-
-    var toRight = function (win) {
-        var rect = win.rect();
-        var breakpoint = rect.x + positions.horizontal.center;
-        if (roughly(breakpoint, positions.horizontal.right + positions.horizontal.center)) {
-            breakpoint = positions.horizontal.left;
+    Rect.prototype.overlap = function (subject) {
+        var tl = Point.max(this.tl, subject.tl);
+        var br = Point.min(this.br, subject.br);
+        if (tl.x > br.x || tl.y > br.y){
+            return null;
         }
-        win.move({
-            x: breakpoint,
-            y: rect.y
-        });
+        return new Rect(tl, br);
     };
 
-    var isRight = function (rect) {
-        return roughly(rect.x, positions.horizontal.right);
+    var getXYWHRect = function (x, y, w, h) {
+        //slate.log( 'getXYWHRect', x, y, w, h);
+        return new Rect(
+            new Point(x, y),
+            new Point( x + w, y + h)
+        );
+    };
+    
+    var getTLBRRect = function (x1, y1, x2, y2){
+        //slate.log( 'getTLBRRect', x1, y1, x2, y2);
+        return new Rect(
+            new Point(x1, y1),
+            new Point(x2, y2)
+        );
     };
 
-    var toLeft = function (win) {
-        var rect = win.rect();
-        var breakpoint = rect.x - positions.horizontal.center;
-        if (breakpoint < positions.horizontal.left) {
-            breakpoint = positions.horizontal.right;
+    var getRectFromSlateObj = function(slob){
+        //slate.log( 'getRectFromSlateObj');
+        return getXYWHRect(
+            slob.x,
+            slob.y,
+            slob.width,
+            slob.height
+        );
+    };
+
+    var rects = {
+        rt: function (view) {
+            return getXYWHRect(
+                view.width / 2,
+                view.y,
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            );
+        },
+        rb: function (view) {
+            return getXYWHRect(
+                view.width / 2,
+                view.height / 2 + 12, // 12 px lower to account for menu bar
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            );
+        },
+        lb: function (view) {
+            return getXYWHRect(
+                view.x,
+                view.height / 2 + 12, // 12 px lower to account for menu bar
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            );
+        },
+        lt: function (view) {
+            return getXYWHRect(
+                view.x,
+                view.y,
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            );
+        },
+        rh: function (view) {
+            return getXYWHRect(
+                view.width / 2,
+                view.y,
+                view.width / 2,
+                view.height
+            );
+        },
+        lh: function (view) {
+            return getXYWHRect(
+                view.x,
+                view.y,
+                view.width / 2,
+                view.height
+            );
+        },
+        fs : getRectFromSlateObj
+    };
+
+    var getNearestMatchingRect = function(win, view){
+        var cursor = { name : null, rect: null, coverage : -1 };
+        for (var name in rects){
+            var rect = rects[name](view);
+            var overlap = win.overlap(rect);
+            if (overlap){
+                var overlapArea = overlap.area();
+                var rectCoverage = overlapArea / rect.area();
+                var winCoverage = overlapArea / win.area();
+                var coverage = rectCoverage + winCoverage;
+                //slate.log(name, coverage);
+                if (coverage > cursor.coverage){
+                    cursor.name = name;
+                    cursor.rect = rect;
+                    cursor.coverage = coverage;
+                    cursor.overlap = overlap;
+                }
+            }
         }
-        win.move({
-            x: breakpoint,
-            y: rect.y
-        });
-    };
-
-    var isLeft = function (rect) {
-        return Math.abs(rect.x - positions.horizontal.left) < 24;
-    };
-
-    var toTop = function (win) {
-        win.move({
-            x: win.rect().x,
-            y: positions.vertical.top
-        });
-    };
-
-    var isTop = function (rect) {
-        return (rect.y < positions.vertical.bottom);
-    };
-
-    var toBottom = function (win) {
-        win.move({
-            x: win.rect().x,
-            y: positions.vertical.bottom
-        });
-    };
-
-    var isBottom = function (rect) {
-        return roughly(rect.y, positions.vertical.bottom);
-    };
-
-    var toMiddle = function (win) {
-        win.move({
-            x: positions.horizontal.center,
-            y: win.rect().y
-        });
-    };
-
-    var isMiddle = function (rect) {
-        return !isRight(rect) && !isLeft(rect);
+        return cursor;
     };
 
     var horizontalPush = function (direction) {
-        return function (win) {
-            view = screen.visibleRect();
-            var rect = win.rect();
-            if (isFull(rect)) {
-                toHalf(win);
-                direction && toLeft(win);
-                return;
-            }
-            if (isQuarter(rect)) {
-                if (isRight(rect)) {
-                    toRight(win);
-                } else {
-                    toLeft(win);
-                }
-                return;
-            }
-            toTop(win);
-            toHalf(win);
-            direction ? toRight(win) : toLeft(win);
+        return function (w) {
+            var screen = slate.screen();
+            var view = screen.rect();
+            var win = getRectFromSlateObj(w.rect());
+            var p = getNearestMatchingRect(win, view);
+            slate.log('nearest match: ' + p.name);
+            // if quarter screen, switch to other quarter
+            // if half screen, switch to other half
+            // else go to indicated half
         };
     };
 
     var verticalPush = function (direction) {
         return function (win) {
-            view = screen.visibleRect();
-            var rect = win.rect();
-            if (direction) { // going down
-                if (isQuarter(rect) && isTop(rect)) {
-                    toBottom(win);
-                    return;
-                }
-                if (isHalf(rect) && !isMiddle(rect)) {
-                    toQuarter(win);
-                    toBottom(win);
-                    return;
-                }
-                if (isFull(rect)) {
-                    toMiddle(win);
-                    toHalf(win);
-                    return;
-                }
-            } else { // going up
-                if (isHalf(rect)) {
-                    if (isRight(rect)) {
-                        toRight(win);
-                    } else if (isLeft(rect)) {
-                        // aww yeah
-                    } else {
-                        toLeft(win);
-                    }
-                    toFull(win);
-                    return;
-                }
-                if (isQuarter(rect)) {
-                    if (isBottom(rect)) {
-                        toTop(win);
-                        return;
-                    } else {
-                        toHalf(win);
-                        return;
-                    }
-                }
-            }
+            //up
+            // if bottom quarter go to top quarter
+            // if top quarter go to half
+            // else go full
+            //down
+            // if half go bottom quarter
         };
     };
 
-    update();
-
     slate.bind("left:ctrl;cmd", horizontalPush(directions.left));
+
+    slate.bind("right:ctrl;cmd", horizontalPush(directions.right));
 
     slate.bind("up:ctrl;cmd", verticalPush(directions.up));
 
     slate.bind("down:ctrl;cmd", verticalPush(directions.down));
-
-    slate.bind("right:ctrl;cmd", horizontalPush(directions.right));
 
 })();
