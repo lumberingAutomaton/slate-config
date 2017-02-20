c78a42bdca89d41162fe24d36f949903d2bf8eed
lumberingAutomaton <lumberingAutomaton@gmail.com>
2017-02-19 21:37:40 -0800

    slate added direction map for easier updates. that allowed remove of direction specific logic. pregenerate and cache rects based on resolution so we can weather resolution changes, but not recalculate every rect every keypress.

--- .slate.js
+++ .slate.js
@@ -2,16 +2,64 @@
 /* globals slate */
 (function () {
     'use strict';
-
-    var similarityFactor = 0.9;
-
-    var direction = {
-        left: 0,
-        right: 1,
-        up: 0,
-        down: 1
+    var cache = {};
+    var similarityFactor = 0.90;
+    var direction = { left: 'left', right: 'right', up: 'up', down: 'down' };
+    var positions = {
+        rt : {
+            left : 'lt',
+            right: 'lt',
+            up: 'rh',
+            down: 'rb'
+        },
+        rb : {
+            left : 'lb',
+            right: 'lb',
+            up: 'rt',
+            down: 'rb'
+        },
+        lb : {
+            left : 'rb',
+            right: 'rb',
+            up: 'lt',
+            down: 'lb'
+        },
+        lt : {
+            left : 'rt',
+            right: 'rt',
+            up: 'lh',
+            down: 'lb'
+        },
+        rh : {
+            left : 'lh',
+            right: 'lh',
+            up: 'fs',
+            down: 'rb'
+        },
+        lh : {
+            left : 'rh',
+            right: 'rh',
+            up: 'lh',
+            down: 'lb'
+        },
+        fs : {
+            left : 'lh',
+            right: 'rh',
+            up: 'fs',
+            down: 'na'
+        },
+        na : {
+            left : 'lh',
+            right: 'rh',
+            up: 'fs',
+            down: 'na'
+        }
     };
 
+
+    /**
+     * Class Point
+     */
     var Point = function (x, y) {
         this.x = x;
         this.y = y;
@@ -31,6 +79,9 @@
         );
     };
 
+    /**
+     * Class Rect
+     */
     var Rect = function (tl, br) {
         this.tl = tl;
         this.br = br;
@@ -51,16 +102,83 @@
         return new Rect(tl, br);
     };
 
+    /**
+     * helper functions
+     */
+    var getPositionRects = function(view){
+        var key = view.width + 'x' + view.height;
+        if (!cache[key]){
+            cache[key] = calculateRects(view);
+        }
+        return cache[key];
+    };
+
+    var calculateRects = function(view){
+        return {
+            rt: getXYWHRect(
+                view.width / 2,
+                view.y,
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            ),
+            rb: getXYWHRect(
+                view.width / 2,
+                view.height / 2 + 12, // 12 px lower to account for menu bar
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            ),
+            lb: getXYWHRect(
+                view.x,
+                view.height / 2 + 12, // 12 px lower to account for menu bar
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            ),
+            lt: getXYWHRect(
+                view.x,
+                view.y,
+                view.width / 2,
+                view.height / 2 - 12 // 12 px smaller to account for menu bar
+            ),
+            rh: getXYWHRect(
+                view.width / 2,
+                view.y,
+                view.width / 2,
+                view.height
+            ),
+            lh: getXYWHRect(
+                view.x,
+                view.y,
+                view.width / 2,
+                view.height
+            ),
+            fs : getRectFromSlateObj(view),
+            na : getXYWHRect(
+                view.width / 5,
+                view.height / 5,
+                (view.width / 5) * 3,
+                (view.height / 5) * 3
+            )
+        };
+    };
+
+    /**
+     * gets a rect object from top left x,y coord
+     * with width and height.
+     */
     var getXYWHRect = function (x, y, w, h) {
-        //slate.log( 'getXYWHRect', x, y, w, h);
+        // slate.log( 'getXYWHRect', x, y, w, h);
         return new Rect(
             new Point(x, y),
             new Point( x + w, y + h)
         );
     };
-    
+
+    /**
+     * gets a rect object from top left x, y coord
+     * and bottom right x, y coord.
+     */
     var getTLBRRect = function (x1, y1, x2, y2){
-        //slate.log( 'getTLBRRect', x1, y1, x2, y2);
+        // slate.log( 'getTLBRRect', x1, y1, x2, y2);
         return new Rect(
             new Point(x1, y1),
             new Point(x2, y2)
@@ -68,7 +186,7 @@
     };
 
     var getRectFromSlateObj = function(slob){
-        //slate.log( 'getRectFromSlateObj');
+        // slate.log( 'getRectFromSlateObj');
         return getXYWHRect(
             slob.x,
             slob.y,
@@ -77,69 +195,19 @@
         );
     };
 
-    var rects = {
-        rt: function (view) {
-            return getXYWHRect(
-                view.width / 2,
-                view.y,
-                view.width / 2,
-                view.height / 2 - 12 // 12 px smaller to account for menu bar
-            );
-        },
-        rb: function (view) {
-            return getXYWHRect(
-                view.width / 2,
-                view.height / 2 + 12, // 12 px lower to account for menu bar
-                view.width / 2,
-                view.height / 2 - 12 // 12 px smaller to account for menu bar
-            );
-        },
-        lb: function (view) {
-            return getXYWHRect(
-                view.x,
-                view.height / 2 + 12, // 12 px lower to account for menu bar
-                view.width / 2,
-                view.height / 2 - 12 // 12 px smaller to account for menu bar
-            );
-        },
-        lt: function (view) {
-            return getXYWHRect(
-                view.x,
-                view.y,
-                view.width / 2,
-                view.height / 2 - 12 // 12 px smaller to account for menu bar
-            );
-        },
-        rh: function (view) {
-            return getXYWHRect(
-                view.width / 2,
-                view.y,
-                view.width / 2,
-                view.height
-            );
-        },
-        lh: function (view) {
-            return getXYWHRect(
-                view.x,
-                view.y,
-                view.width / 2,
-                view.height
-            );
-        },
-        fs : getRectFromSlateObj
-    };
-
-    var getNearestMatchingRect = function(win, view){
+    var getNearestMatchingRect = function(win, rects){
         var cursor = { name : null, rect: null, coverage : -1 };
         for (var name in rects){
-            var rect = rects[name](view);
+            var rect = rects[name];
             var overlap = win.overlap(rect);
             if (overlap){
                 var overlapArea = overlap.area();
+                // how much of the position is covered by win?
                 var rectCoverage = overlapArea / rect.area();
+                // how much of the win is over the position?
                 var winCoverage = overlapArea / win.area();
                 var coverage = rectCoverage + winCoverage;     
-                //slate.log(name, coverage);
+                // slate.log(name, coverage);
                 if (coverage > cursor.coverage){
                     cursor.name = name;
                     cursor.rect = rect;
@@ -151,55 +219,36 @@
         return cursor;
     };
 
-    var getCurrentPos = function(w){
-        var screen = slate.screen();
-        var view = screen.rect();
-        var win = getRectFromSlateObj(w.rect());
-        var pos = getNearestMatchingRect(win, view);
+    var getCurrentPos = function(win, rects){
+        var winRect = getRectFromSlateObj(win.rect());
+        var match = getNearestMatchingRect(winRect, rects);
         // if the overlap is similar enough to the position, the window is in that position.
-        if (pos.rect.area() * similarityFactor > pos.overlap.area()){
-            return null;
+        if (match.rect.area() * similarityFactor > match.overlap.area()){
+            match.name = 'na';
+            match.rect = rects.na;
         }
-        return pos;
+        return match;
     };
 
-    var horizontalPush = function (direction) {
-        return function (w) {
-            var pos = getCurrentPos(w);
-            if (pos){
-                slate.log('nearest match accepted > ' + pos.name);
-            } else {
-                slate.log('nearest match discarded');
-            }
-            // if quarter screen, switch to other quarter
-            // if half screen, switch to other half
-            // else go to indicated half
-        };
-    };
-
-    var verticalPush = function (direction) {
+    var keypress = function (direction) {
         return function (win) {
-            var pos = getCurrentPos(w);
-            if (pos){
-                slate.log('nearest match accepted > ' + pos.name);
-            } else {
-                slate.log('nearest match discarded');
-            }
-            //up
-            // if bottom quarter go to top quarter
-            // if top quarter go to half
-            // else go full
-            //down
-            // if half go bottom quarter
+            var screen = slate.screen();
+            var view = screen.rect();
+            var rects = getPositionRects(view);
+            var pos = getCurrentPos(win, rects);
+            var next = positions[pos.name][direction];
+            slate.log(direction + ': ' + pos.name + '>' + next);
+            move(win, rects[next]);
+            
         };
     };
 
-    slate.bind("left:ctrl;cmd", horizontalPush(direction.left));
+    slate.bind("left:ctrl;cmd", keypress(direction.left));
 
-    slate.bind("right:ctrl;cmd", horizontalPush(direction.right));
+    slate.bind("right:ctrl;cmd", keypress(direction.right));
 
-    slate.bind("up:ctrl;cmd", verticalPush(direction.up));
+    slate.bind("up:ctrl;cmd", keypress(direction.up));
 
-    slate.bind("down:ctrl;cmd", verticalPush(direction.down));
+    slate.bind("down:ctrl;cmd", keypress(direction.down));
 
 })();
